af43e0bbff1df34e0118b13789a0b75e0630b086
vaniaacevedo <vaniaacevedo@me.com>
2018-03-09 00:07:21 +0100

    created the first views for creating and editing the posts

---
+++ trydjango/posts/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.core.urlresolvers import reverse
+# Create your models here.
+
+class Post(models.Model):
+	title= models.CharField(max_length=120)
+	content= models.TextField()
+	timestamp= models.DateTimeField(auto_now=False, auto_now_add=True)
+	updated= models.DateTimeField(auto_now=True, auto_now_add=False)
+
+
+	def __str__(self):
+		return self.title
+
+	def get_absolute_url(self):
+		return reverse('posts:post_detail', kwargs={'id':self.id})
+		# return '/posts/%s/' %(self.id)
---
+++ trydjango/posts/views.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
+
+from .models import Post
+from .forms import PostForm
+
+# Create your views here.
+
+def post_create(request):
+	form= PostForm(request.POST or None) #initialization
+	if form.is_valid():
+		instance= form.save(commit=False)
+		instance.save()
+		return HttpResponseRedirect(instance.get_absolute_url())
+	context= { 'form':form }
+	return render(request, 'post_form.html', context)
+
+def post_detail(request, id):
+	instance= get_object_or_404(Post, id=id)
+	context={ 'title': instance.title, 'instance':instance, }
+	return render(request, 'post_detail.html', context)
+
+def post_list(request):
+	queryset= Post.objects.all()
+	context= { 'title':'List','object_list':queryset, }
+	return render(request, 'index.html', context)
+
+def post_update(request, id=None):
+	instance= get_object_or_404(Post, id=id)
+	form= PostForm(request.POST or None, instance=instance)
+	if form.is_valid():
+		instance= form.save(commit=False)
+		instance.save()
+		return HttpResponseRedirect(instance.get_absolute_url())
+	context= { 'title': instance.title, 'instance': instance, 'form': form, }	
+	return render( request, 'post_form.html', context )
+	
+
+def post_delete(request):
+	return HttpResponse('<h1>Delete</h1>')
+
+
